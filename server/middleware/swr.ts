48b4fe4715b9c24c2d89a9fe7e23e232cb6bf16b
zernonia <zernonia@gmail.com>
2022-06-29 19:52:31 +0800

    update swr to 1mins

--- server/middleware/swr.ts
+++ server/middleware/swr.ts
@@ -2,8 +2,8 @@ export default defineEventHandler(async (event) => {
   let url = event.req.url
   console.log(url)
   if (url.includes("/widget/")) {
-    // cache 15mins
-    event.res.setHeader("Cache-Control", "s-maxage=900, stale-while-revalidate")
+    // cache 1mins
+    event.res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate")
   } else if (url.includes("/dashboard")) {
   } else {
     event.res.setHeader("Cache-Control", "s-maxage=7200, stale-while-revalidate")
