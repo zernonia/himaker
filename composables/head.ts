ce9d2e4d8afc75f3fa3a7c3b28e2ccb6b23ce60e
zernonia <zernonia@gmail.com>
2022-06-29 20:05:27 +0800

    fix og

--- composables/head.ts
+++ composables/head.ts
@@ -4,23 +4,23 @@ export const useCustomHead = (title: string, description?: string, image?: strin
     meta: [
       {
         name: "description",
-        content: description ?? "Easily share your social link, project, or any link with your visitors",
+        content: description ?? "Easily show your social links, projects, or any links with your audience.",
       },
       { name: "twitter:card", content: "summary_large_image" },
       { name: "twitter:site", content: "@zernonia" },
-      { name: "twitter:title", content: title },
+      { name: "twitter:title", content: `${title} | Hi Maker 👋🏻 ` },
       {
         name: "twitter:description",
-        content: description ?? "Easily share your social link, project, or any link with your visitors",
+        content: description ?? "Easily show your social links, projects, or any links with your audience.",
       },
       { name: "twitter:image", content: image ?? "https://himaker.io/og.png" },
       { property: "og:type", content: "website" },
-      { property: "og:title", content: title },
+      { property: "og:title", content: `${title} | Hi Maker 👋🏻 ` },
       { property: "og:url", content: "https://himaker.io" },
       { property: "og:image", content: image ?? "https://himaker.io/og.png" },
       {
         property: "og:description",
-        content: description ?? "Easily share your social link, project, or any link with your visitors",
+        content: description ?? "Easily show your social links, projects, or any links with your audience.",
       },
     ],
   })
