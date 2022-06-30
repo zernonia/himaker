ac3ebe465714ee1f62bc96bddcf65f49d2fa4fc3
zernonia <zernonia@gmail.com>
2022-06-30 09:50:35 +0800

    add contact email

--- pages/contact.vue
+++ pages/contact.vue
@@ -4,5 +4,10 @@
   <div class="mx-auto prose">
     <h3>You know how to contact me right?</h3>
     <h4>Check the bottom-left corner! 😉</h4>
+    <br />
+    <h4>
+      Or reach out to me at
+      <a class="!text-primary-hover !font-semibold ml-2" href="mailto: admin@himaker.io"> admin@himaker.io</a>
+    </h4>
   </div>
 </template>
